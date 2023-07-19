d86a2f79145e8aa5e858c2019cb9e27b6d5b2c8b
mahfuzur <mdmahfuazur7788@gamil.com>
2023-07-19 20:37:01 +0600

    File System

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // File system 
 const fs = require('fs')
 let text = fs.readFileSync('./files/input.txt', 'utf-8');
+//console.log(text)
 
-text=`Copied Text: ${text}`;
+text = `Copied Text File OO: ${text}`;
 
-fs.writeFileSync('./files/output.txt', text)
+fs.writeFileSync("./files/output.txt", text);
