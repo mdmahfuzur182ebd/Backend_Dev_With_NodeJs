f3257f4f1bd659348ab2731ebe86dc398bc88509
mahfuzur <mdmahfuazur7788@gamil.com>
2023-07-19 18:06:24 +0600

    Crating module

---
+++ circle.js
@@ -0,0 +1,8 @@
+//console.log(module)
+const area = (r) => 3.14 * r * r;
+const circumference = (r) => 2 * 3.14 * r;
+module.exports.name ="Hello Sajib"
+
+module.exports.some = area;
+module.exports.circumference = circumference;
+console.log(module)
