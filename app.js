e4b1afa9ba445c8a4351e841f3186dbbdb3b1552
mahfuzur <mdmahfuazur7788@gamil.com>
2023-07-19 21:22:54 +0600

    Http module

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-// Inheriting EventEmitter class
+// HTTP Module
+const http = require("http");
 
-const  MyEvent = require('./myevent')
-const myEvent = new MyEvent();
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("Hello world");
+    res.end()
+  }
+  if(req.url === '/students'){
+    res.write(JSON.stringify([
+        {name: "some"},
+        {name: "Thing"}
+    ]))
+    res.end()
+  }
+});
 
-myEvent.on('event1', ()=>{
-    console.log("Event Function 1")
-})
-
-myEvent.functionSome()
+server.listen(3000); //3000-> Port Number.
+console.log("Listening on Port 3000.....")
--- myevent.js
+++
@@ -1,9 +0,0 @@
-const EventEmitter = require("events");
-
-class MyEvent extends EventEmitter {
-  functionSome = () => {
-    this.emit("event1");
-  };
-}
-
-module.exports = MyEvent;
